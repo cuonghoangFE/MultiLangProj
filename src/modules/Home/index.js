2e69ea2d94bcdd812bec80def2ac92fa9a14e6eb
Cuong Hoang Cao <cuong.hoang@pycogroup.com>
2019-10-03 16:15:30 +0700

    update config

--- src/modules/Home/index.js
+++ src/modules/Home/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import * as React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 // import { withTranslation } from 'react-i18next';
-import { withTranslation } from '../Language';
+import { withTranslation, TFunction } from '../Language';
 import { compose } from 'recompose'
 import withNavigationEventsHOC from '../Navigation/withNavigationEvents';
 
-const HomeScreen = ({ t, language, updateLanguage }) => {
+interface TProps {
+  t: TFunction
+  language: String
+  updateLanguage: Function
+}
+
+const HomeScreen = ({ t, language, updateLanguage }: TProps) => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
@@ -14,7 +20,7 @@ const HomeScreen = ({ t, language, updateLanguage }) => {
         <Text>{t('change')}</Text>
       </TouchableOpacity>
       <View style={{height: 50}} />
-      <Text style={{ fontSize: 20, marginBottom: 20 }}>{t('hello', { world: language === 'it' ? 'världen' : (language === 'vi' ? 'the gioi' : 'world') } )}</Text>
+      <Text style={{ fontSize: 20, marginBottom: 20 }}>{t('hello', { world: language === 'en' ? 'en world' : (language === 'vi' ? 'vi world' : 'it world')}) } )}</Text>
     </View>
   );
 }
--- src/modules/Language/index.js
+++ src/modules/Language/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { TouchableOpacity, Text } from 'react-native';
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
@@ -19,6 +20,20 @@ const regexpStr = "".concat(prefix, "(.+?)").concat(suffix);
 const regexpUnescapeStr = "".concat(prefix).concat(unescapePrefix, "(.+?)").concat(unescapeSuffix).concat(suffix);
 const nestingRegexpStr = "".concat(nestingPrefix, "(.+?)").concat(nestingSuffix);
 
+type TVarMap = { [s: string]: string };
+
+export interface TFunction {
+  // basic usage
+  <
+    TResult = string,
+    TKeys extends TFunctionKeys = string,
+    TInterpolationMap extends TVarMap = object
+  >(
+    i18nKey: TKeys,
+    values?: TInterpolationMap,
+  ): TResult;
+}
+
 const multiIndex = (obj,is) => {
   return is.length ? multiIndex(obj[is[0]],is.slice(1)) : obj
 }
@@ -33,17 +48,26 @@ export class LanguageProvider extends React.Component {
 
   updateLanguage = e => this.setState({ language: e });
 
-  t = (i18nKey, values) => {
+  t: TFunction = (i18nKey, values) => {
     const { language } = this.state;
     const langKey = language === 'en' ? en : (language === 'vi' ? vi : it);
     let text = pathIndex(langKey,i18nKey);
-    if (!text) return i18nKey;
-    if (values) {
-      for (let i in values) {
-        const regexpStr = "".concat(prefix, i).concat(suffix);
-        const regexp = new RegExp(regexpStr, 'g');
-        text = text.replace(regexp, values[i]);
-      }
+    const isHasVarRegex = text.match(new RegExp(regexpStr, 'g'));
+    if (!text) {
+      console.warn(`Key ${i18nKey} not found`);
+      return text;
+    }
+    if (isHasVarRegex !== null &&
+      isHasVarRegex.length > 0 &&
+      !values
+    ) {
+      isHasVarRegex.map(keyMissing => console.warn(`Missing key ${keyMissing} in ${language}[${i18nKey}]}`));
+      return text;
+    }
+    for (let i in values) {
+      const regexpStr = "".concat(prefix, i).concat(suffix);
+      const regexp = new RegExp(regexpStr, 'g');
+      text = text.replace(regexp, values[i]);
     }
     return text;
   }
